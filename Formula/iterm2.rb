d3783e9c218b33b1c3c4de6e44187130c5db7eb5
Prashant Sinha <prashant+git@noop.pw>
2016-12-12 12:45:17 +0530

    Latest iTerm build has margins configurable!

--- Formula/iterm2.rb
+++ Formula/iterm2.rb
@@ -3,19 +3,17 @@ class Iterm2 < Formula
   homepage "https://www.iterm2.com/"
   head "https://github.com/gnachman/iTerm2.git"
 
-  option "with-padding", "Skip removing padding."
-
   depends_on :xcode
   depends_on :arch => :x86_64
 
   def install
-    if not build.with? "padding"
-      ohai "Patching Margins"
-      inreplace "sources/iTermTextDrawingHelper.h" do |s|
-        s.gsub! "#define MARGIN 5", "#define MARGIN 0"
-        s.gsub! "#define VMARGIN 2", "#define VMARGIN 0"
-      end
-    end
+#    if not build.with? "padding"
+#      ohai "Patching Margins"
+#      inreplace "sources/iTermTextDrawingHelper.h" do |s|
+#        s.gsub! "#define MARGIN 5", "#define MARGIN 0"
+#        s.gsub! "#define VMARGIN 2", "#define VMARGIN 0"
+#      end
+#    end
     system "make", "prod"
     prefix.install "build/Deployment/iTerm2.app"
   end
