f9fdd864309c6729f806a627a893cda5b6707c09
Prashant Sinha <prashant+git@noop.pw>
2017-01-08 17:40:49 +0530

    Update to ed25519 release

--- Formula/libed25519.rb
+++ Formula/libed25519.rb
@@ -1,8 +1,8 @@
 class Libed25519 < Formula
   desc "Library for Ed25519"
   homepage "https://github.com/prashnts/ed25519"
-  url "https://github.com/prashnts/ed25519/archive/0.1.1.tar.gz"
-  sha256 "8f839597968cb6a0f0af077fb857933a46ccaffbd4bd505a442a804afbcc29e0"
+  url "https://github.com/prashnts/ed25519/archive/0.1.2.tar.gz"
+  sha256 "af48084b5c8377b19a020a419aea74443eecdfb069e6239e2a102081e0e17c23"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
