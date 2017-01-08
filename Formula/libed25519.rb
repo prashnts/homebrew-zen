15e3048ca1020a0e988786d49b3ba9bc0dd571a5
Prashant Sinha <prashant+git@noop.pw>
2017-01-08 17:29:45 +0530

    Fix SHA 256 checksum

--- Formula/libed25519.rb
+++ Formula/libed25519.rb
@@ -2,7 +2,7 @@ class Libed25519 < Formula
   desc "Library for Ed25519"
   homepage "https://github.com/prashnts/ed25519"
   url "https://github.com/prashnts/ed25519/archive/0.1.0.tar.gz"
-  sha256 "9d3072fb2bb1932475376f03a31014eee8cba5396d81655989e8e9a20c7e281b"
+  sha256 "4a7808900bfb9cbef513a4ea7c78e37062dd46940ea3f9daaf9aa7ed4b2c1ca1"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
